1743c9b4c35bc71c47a25f03a498ebc681c79167
Ikhsan Bahar <ikhsanbahar@gmail.com>
2022-10-28 18:37:27 +0700

    Perbaikan Tampilan Nomor Rekma Induk.

--- application/models/get_mata_anggaran_individual_benchmarks.php
+++ application/models/get_mata_anggaran_individual_benchmarks.php
@@ -43,8 +43,8 @@ class get_mata_anggaran_individual_benchmarks extends CI_Model {
 
         $query_total = $this->db->query($sql_total);
         $total = $query_total->num_rows();
-
-        $sql = "select b.rekmakode as id, b.kode, CONCAT(substring(b.rekmakode from 1 for 3), '.', substring(b.rekmakode from 4 for 3), '.', substring(b.rekmakode from 7 for 2)) rekmakode, b.rekmanama, CONCAT(substring(b.rekmainduk from 1 for 3), '.', substring(b.rekmainduk from 4 for 3), '.', substring(b.rekmainduk from 7 for 2)) rekmainduk, (select a.rekmainduknama from tblmastermainduk a where a.rekmainduk = b.rekmainduk) rekmainduknama, b.benchmarkang, b.benchmarkprog from tblmastermaindividual b ".$clouse.$this->where_project($clouse)." order by id asc, kode asc, rekmainduk asc, rekmakode asc offset $iDisplayStart limit $iDisplayLength";
+        // CONCAT(substring(b.rekmainduk from 1 for 3), '.', substring(b.rekmainduk from 4 for 3), '.', substring(b.rekmainduk from 7 for 2)) 
+        $sql = "select b.rekmakode as id, b.kode, CONCAT(substring(b.rekmakode from 1 for 3), '.', substring(b.rekmakode from 4 for 3), '.', substring(b.rekmakode from 7 for 2)) rekmakode, b.rekmanama, b.rekmainduk, (select a.rekmainduknama from tblmastermainduk a where a.rekmainduk = b.rekmainduk) rekmainduknama, b.benchmarkang, b.benchmarkprog from tblmastermaindividual b ".$clouse.$this->where_project($clouse)." order by id asc, kode asc, rekmainduk asc, rekmakode asc offset $iDisplayStart limit $iDisplayLength";
 
         $page = ($iDisplayStart / $iDisplayLength);
 
